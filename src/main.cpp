8e0b49486dfe3591805ee981c08256e978a9117e
TTTriplicate <tttriplicate@gmail.com>
2021-02-01 16:22:21 -0500

    better, faster slalom.  Checked sensor L:R  margin of error, it is about 1.2% on battery, 3.9% on USB

--- src/main.cpp
+++ src/main.cpp
@@ -4,47 +4,55 @@
 
 Encoders hall_sensor;
 
-void output(){
+void output()
+{
   printf("hall sensor reads\n Left : %d \t Right : %d\n", hall_sensor.get_countA(), hall_sensor.get_countB());
 }
 
-int main() {
+int main()
+{
 
   // put your setup code here, to run once:
   BufferedSerial pc(USBTX, USBRX);
   Motion robo;
 
   bool slalom = 1;
+  int turnDist = (192 * 3);
   printf("code running...");
 
-  robo.drive();
-  ThisThread::sleep_for(1s);
-  while(1) {
-    output();
-    // put your main code here, to run repeatedly:
-    if(slalom){
-      if (hall_sensor.get_countA() > 1000){
+  robo.drive(.5);
+  while (1)
+  {
+    //output();
+    //put your main code here, to run repeatedly:
+    if (slalom)
+    {
+      if (hall_sensor.get_countA() > turnDist)
+      {
         hall_sensor.resetB();
         slalom = 0;
-        robo.leftFWD(.4);
-        robo.rightFWD(.25);
+        robo.leftFWD(.8);
+        robo.rightFWD(.66);
       }
-      else{
+      else
+      {
         ThisThread::sleep_for(200);
       }
     }
-    else{
-      if (hall_sensor.get_countB() > 1000){
+    else
+    {
+      if (hall_sensor.get_countB() > turnDist)
+      {
         hall_sensor.resetA();
         slalom = 1;
-        robo.leftFWD(.25);
-        robo.rightFWD(.4);
+        robo.rightFWD(.8);
+        robo.leftFWD(.66);
       }
-      else{
+      else
+      {
         ThisThread::sleep_for(200);
       }
-
     }
   }
-  output();
+  //output();
 }
