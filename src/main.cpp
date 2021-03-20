d177e0c5e348ff287be1f46ef0022250584f7cbf
TTTriplicate <tttriplicate@gmail.com>
2021-03-20 15:42:09 -0400

    corrected formula for turns

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ void turnRadiansRight(float radians)
 void turnRadiansLeft(float radians)
 {
     int wheelDiameterMm = 66;
-    float arcLength = radians * (182 / 2);
+    float arcLength = radians * (182);
     printf("Arc length: \t %d\n", static_cast<int>(arcLength));
     float rotations = (arcLength / (wheelDiameterMm * M_PI));
     int polaritySwaps = (rotations * 192);
